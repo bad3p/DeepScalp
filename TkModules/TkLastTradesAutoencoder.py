Fixed KL term in VAEs. Autoencoders uses SSIM loss in training. Autoencoder data preprocessor improved.

TkModules/TkLastTradesAutoencoder.py
@@ -44,11 +44,12 @@ class TkLastTradesAutoencoder(torch.nn.Module):
     def forward(self, input):
         y = self._encoder( input )
         self._mean, self._logvar = self._mean_layer(y), self._logvar_layer(y)
+        self._logvar = self._logvar.clamp( -5, 5 )
         self._code = torch.cat( (self._mean, self._logvar), dim=1 )
 
-        epsilon = torch.randn_like(self._logvar).to(y.device)  
-        z = self._mean + torch.exp(0.5 * self._logvar) * epsilon
+        z = self._mean + torch.randn_like( torch.exp(0.5 * self._logvar) )
         z = self._reparametrization_layer(z)
         z = self._decoder( z )
+        z = z.clamp( 0, 1 )
         
         return z, self._mean, self._logvar

TkModules/TkOrderbookAutoencoder.py
@@ -20,7 +20,7 @@ class TkOrderbookAutoencoder(torch.nn.Module):
         self._code_scale = float( _cfg['Autoencoders']['OrderbookAutoencoderCodeScale'] )        
 
         self._mean_layer = torch.nn.Linear(self._hidden_layer_size, self._code_layer_size)
-        self._logvar_layer = torch.nn.Linear(self._hidden_layer_size, self._code_layer_size)
+        self._logvar_layer = torch.nn.Linear(self._hidden_layer_size, self._code_layer_size)        
         self._reparametrization_layer = torch.nn.Linear(self._code_layer_size, self._hidden_layer_size)
 
     def code_layer_size(self):
@@ -37,11 +37,12 @@ class TkOrderbookAutoencoder(torch.nn.Module):
     def forward(self, input):
         y = self._encoder( input )
         self._mean, self._logvar = self._mean_layer(y), self._logvar_layer(y)
+        self._logvar = self._logvar.clamp( -5, 5 )
         self._code = torch.cat( (self._mean, self._logvar), dim=1 )
-
-        epsilon = torch.randn_like(self._logvar).to(y.device)  
-        z = self._mean + torch.exp(0.5 * self._logvar) * epsilon
+        
+        z = self._mean + torch.randn_like( torch.exp(0.5 * self._logvar) )
         z = self._reparametrization_layer(z)
         z = self._decoder( z )
+        z = z.clamp( 0, 1 )
         
         return z, self._mean, self._logvar

TkModules/TkTimeSeriesForecaster.py
@@ -4,6 +4,7 @@ import torch
 import json
 from TkModules.TkModel import TkModel
 from TkModules.TkStackedLSTM import TkStackedLSTM
+from TkModules.TkSelfAttention import TkSelfAttention
 
 # --------------------------------------------------------------------------------------------------------------
 # Time series forecasting model
@@ -38,10 +39,10 @@ class TkTimeSeriesForecaster(torch.nn.Module):
 
         self._lstm = torch.nn.ModuleList( self._lstm )
         
-        self._mlp_input_tensors = None
+        self._lstm_output_tensors = None
 
-    def mlp_input(self):
-        return self._mlp_input_tensors
+    def lstm_output(self):
+        return self._lstm_output_tensors
 
     def input_slice(self, idx:int):
         return self._input_slice_tensors[idx]
@@ -53,7 +54,7 @@ class TkTimeSeriesForecaster(torch.nn.Module):
         input = torch.reshape( input, ( batch_size, self._prior_steps_count, self._input_width) )
 
         self._input_slice_tensors = []
-        self._mlp_input_tensors = []
+        self._lstm_output_tensors = []
 
         for i in range(len(self._input_slices)):
             ch0 = self._input_slices[i][0]
@@ -61,13 +62,13 @@ class TkTimeSeriesForecaster(torch.nn.Module):
             slice_size = ch1 - ch0
             input_slice_tensor = input[:, :, ch0:ch1]
             self._input_slice_tensors.append( input_slice_tensor )
-            self._mlp_input_tensors.append( self._lstm[i](input_slice_tensor) )
+            self._lstm_output_tensors.append( self._lstm[i](input_slice_tensor) )
         
-        self._mlp_input_tensors = tuple(self._mlp_input_tensors)
-        self._mlp_input_tensors = torch.cat( self._mlp_input_tensors, dim=1) 
+        self._lstm_output_tensors = tuple(self._lstm_output_tensors)
+        self._lstm_output_tensors = torch.cat( self._lstm_output_tensors, dim=1) 
 
-        y = torch.reshape( self._mlp_input_tensors, (batch_size, self._mlp_input_size) )
-        y = self._mlp.forward( y )
+        self._lstm_output_tensors = torch.reshape( self._lstm_output_tensors, (batch_size, self._mlp_input_size) )
+        y = self._mlp.forward( self._lstm_output_tensors )
         y = torch.reshape( y, ( batch_size, self._target_code_width) )
 
         return y

TkPreprocessAutoencoderData.py
@@ -7,6 +7,7 @@ import json
 import copy
 import random
 import math
+import gc
 from os import listdir
 from os.path import isfile, join
 from datetime import date, datetime, timezone
@@ -210,67 +211,19 @@ class TkAutoencoderDataPreprocessor():
         elapsed_time = end_time - start_time
         return raw_sample_count / elapsed_time
 
-    def add_preprocessed_samples(self, orderbook_samples : list, last_trades_samples : list, training_category : bool, render_callback):
-
-        self._normalized_orderbook_samples = []
-        self._normalized_last_trades_samples = []
-
-        callback_indices = [int(i / 100.0 * len(orderbook_samples)) for i in range(1,100)]
-
-        for i in range(len(orderbook_samples)):
-            distribution = orderbook_samples[i]            
-            lsh_query = self._orderbook_lsh.query( distribution, num_results=1 )
-            if len(lsh_query) == 0 or lsh_query[0][1] > self._orderbook_sample_similarity:
-                self._normalized_orderbook_samples.append(distribution)
-                self._orderbook_lsh.index(distribution)
-            if len(callback_indices) > 0 and i >= callback_indices[0]:
-                del callback_indices[0]
-                if render_callback != None:
-                    render_callback( self._normalized_orderbook_samples, self._normalized_last_trades_samples )
-
-        callback_indices = [int(i / 100.0 * len(last_trades_samples)) for i in range(1,100)]
-
-        for i in range(len(last_trades_samples)):
-            distribution = last_trades_samples[i]            
-            lsh_query = self._last_trades_lsh.query( distribution, num_results=1 )
-            if len(lsh_query) == 0 or lsh_query[0][1] > self._last_trades_sample_similarity:
-                self._normalized_last_trades_samples.append(distribution)
-                self._last_trades_lsh.index(distribution)
-            if len(callback_indices) > 0 and i >= callback_indices[0]:
-                del callback_indices[0]
-                if render_callback != None:
-                    render_callback( self._normalized_orderbook_samples, self._normalized_last_trades_samples )
-
-        if training_category:                    
-            self._orderbook_training_data_offset = self.write_samples( 
-                self._normalized_orderbook_samples,
-                self._orderbook_training_index, 
-                self._orderbook_training_data_offset, 
-                self._orderbook_training_data_stream
-            )
-            self._last_trades_training_data_offset = self.write_samples(
-                self._normalized_last_trades_samples, 
-                self._last_trades_training_index, 
-                self._last_trades_training_data_offset, 
-                self._last_trades_training_data_stream
-            )
-        else:
-            self._orderbook_test_data_offset = self.write_samples( 
-                self._normalized_orderbook_samples,
-                self._orderbook_test_index, 
-                self._orderbook_test_data_offset, 
-                self._orderbook_test_data_stream
-            )
-            self._last_trades_test_data_offset = self.write_samples(
-                self._normalized_last_trades_samples, 
-                self._last_trades_test_index, 
-                self._last_trades_test_data_offset, 
-                self._last_trades_test_data_stream
-            )
-
     def generate_synthetic_samples(self, training_category : bool, render_callback):
 
+        def generate_render_callback_indices(num_samples:int):
+            if num_samples < 1000:
+                return [int(i / 100.0 * num_samples) for i in range(1,100)]
+            elif num_samples < 10000:
+                return [int(i / 1000.0 * num_samples) for i in range(1,1000)]
+            else:
+                return [int(i / 10000.0 * num_samples) for i in range(1,10000)]
+
         synthetic_sample_ratio = float(config['Autoencoders']['SyntheticSampleRatio'])
+
+        lsh_reset_threshold_time = 2.0
 
         if synthetic_sample_ratio <= 0.0:
             return
@@ -279,15 +232,6 @@ class TkAutoencoderDataPreprocessor():
         num_orderbook_test_samples = len(self._orderbook_test_index)
         num_last_trades_training_samples = len(self._last_trades_training_index)
         num_last_trades_test_samples = len(self._last_trades_test_index)
-
-        num_samples = 0
-
-        if training_category:
-            num_samples = int( synthetic_sample_ratio * max( num_orderbook_training_samples, num_last_trades_training_samples ) )
-        else:
-            num_samples = int( synthetic_sample_ratio * max( num_orderbook_test_samples, num_last_trades_test_samples ) )
-            if num_samples == 0:
-                num_samples = int( synthetic_sample_ratio * max( num_orderbook_training_samples, num_last_trades_training_samples ) )
 
         synthetic_orderbook_scheme = json.loads(config['Autoencoders']['SyntheticOrderbookScheme'])
         if not type(synthetic_orderbook_scheme) is list:
@@ -304,40 +248,73 @@ class TkAutoencoderDataPreprocessor():
         self._normalized_orderbook_samples = []
         self._normalized_last_trades_samples = []
 
-        discarded_orderbook_samples = 0
-        discarded_last_trades_samples = 0
+        num_samples = 0
 
-        callback_indices = [int(i / 100.0 * num_samples) for i in range(1,100)]
+        if training_category:
+            num_samples = int( synthetic_sample_ratio * num_orderbook_training_samples )
+        else:
+            if num_orderbook_test_samples > 0:
+                num_samples = int( synthetic_sample_ratio * num_orderbook_test_samples )
+            else:
+                num_samples = int( synthetic_sample_ratio * num_orderbook_training_samples )
+
+        callback_indices = generate_render_callback_indices( num_samples )
 
         for i in range(num_samples):
-            orderbook_scheme = synthetic_orderbook_scheme[random.randint(0, len(synthetic_orderbook_scheme)-1)]
-            distribution = np.zeros( self._orderbook_width, dtype=float)
-            distribution[int(self._orderbook_width/2)-2] = random.uniform(0.0, synthetic_orderbook_sample_central_bias) # center synthetic orderbook sample to the maximal ask price
-            TkStatistics.generate_distribution( distribution, orderbook_scheme, synthetic_sample_bias )
-            TkStatistics.to_cumulative_distribution(distribution)
-            lsh_query = self._orderbook_lsh.query( distribution, num_results=1 )
-            if len(lsh_query) == 0 or lsh_query[0][1] > self._synthetic_orderbook_sample_similarity:
-                self._normalized_orderbook_samples.append(distribution)
-                self._orderbook_lsh.index(distribution)
-            else:
-                discarded_orderbook_samples = discarded_orderbook_samples + 1
 
-            last_trades_scheme = synthetic_last_trades_scheme[random.randint(0, len(synthetic_last_trades_scheme)-1)]
-            distribution = np.zeros( self._last_trades_width, dtype=float)
-            TkStatistics.generate_clustered_distribution( distribution, last_trades_scheme, synthetic_sample_bias, synthetic_last_trades_sample_max_variance )
-            lsh_query = self._last_trades_lsh.query( distribution, num_results=1 )
-            if len(lsh_query) == 0 or lsh_query[0][1] > self._synthetic_last_trades_sample_similarity:
-                self._normalized_last_trades_samples.append(distribution)
-                self._last_trades_lsh.index(distribution)
-            else:
-                discarded_last_trades_samples = discarded_last_trades_samples + 1
+            t0 = time.time()
+            success = False
+            while not success:
+                orderbook_scheme = synthetic_orderbook_scheme[random.randint(0, len(synthetic_orderbook_scheme)-1)]
+                distribution = np.zeros( self._orderbook_width, dtype=float)
+                distribution[int(self._orderbook_width/2)-2] = random.uniform(0.0, synthetic_orderbook_sample_central_bias) # center synthetic orderbook sample to the maximal ask price
+                TkStatistics.generate_distribution( distribution, orderbook_scheme, synthetic_sample_bias )
+                TkStatistics.to_cumulative_distribution(distribution)
+                lsh_query = self._orderbook_lsh.query( distribution, num_results=1 )
+                if len(lsh_query) == 0 or lsh_query[0][1] > self._synthetic_orderbook_sample_similarity:
+                    self._normalized_orderbook_samples.append(distribution)
+                    self._orderbook_lsh.index(distribution)
+                    success = True
+            t1 = time.time()
+            if t1 - t0 > lsh_reset_threshold_time:
+                self._orderbook_lsh = LSHash(self._lshash_size, self._orderbook_width)
 
             if len(callback_indices) > 0 and i >= callback_indices[0]:
                 del callback_indices[0]
                 if render_callback != None:
-                    render_callback( self._normalized_orderbook_samples, self._normalized_last_trades_samples )
+                    render_callback( self._normalized_orderbook_samples, None )
 
-        print('Generated samples: ', num_samples-discarded_orderbook_samples, ',', num_samples-discarded_last_trades_samples )
+        if training_category:
+            num_samples = int( synthetic_sample_ratio * num_last_trades_training_samples )
+        else:
+            if num_orderbook_test_samples > 0:
+                num_samples = int( synthetic_sample_ratio * num_last_trades_test_samples )
+            else:
+                num_samples = int( synthetic_sample_ratio * num_last_trades_training_samples )
+
+        callback_indices = generate_render_callback_indices( num_samples )
+
+        for i in range(num_samples):
+
+            t0 = time.time()
+            success = False
+            while not success:
+                last_trades_scheme = synthetic_last_trades_scheme[random.randint(0, len(synthetic_last_trades_scheme)-1)]
+                distribution = np.zeros( self._last_trades_width, dtype=float)
+                TkStatistics.generate_clustered_distribution( distribution, last_trades_scheme, synthetic_sample_bias, synthetic_last_trades_sample_max_variance )
+                lsh_query = self._last_trades_lsh.query( distribution, num_results=1 )
+                if len(lsh_query) == 0 or lsh_query[0][1] > self._synthetic_last_trades_sample_similarity:
+                    self._normalized_last_trades_samples.append(distribution)
+                    self._last_trades_lsh.index(distribution)
+                    success = True
+            t1 = time.time()
+            if t1 - t0 > lsh_reset_threshold_time:
+                self._last_trades_lsh = LSHash(self._lshash_size, self._last_trades_width)
+
+            if len(callback_indices) > 0 and i >= callback_indices[0]:
+                del callback_indices[0]
+                if render_callback != None:
+                    render_callback( None, self._normalized_last_trades_samples )
 
         if render_callback != None:
             render_callback( self._normalized_orderbook_samples, self._normalized_last_trades_samples )
@@ -475,22 +452,26 @@ with Client(TOKEN, target=INVEST_GRPC_API) as client:
             dpg.set_value("last_trades_samples", str(preprocessor.num_last_trades_samples())+"/"+str(total_samples) )
             dpg.set_value("samples_per_second", str( cumulative_samples_per_second/samples_per_second_norm ) )
 
+            gc.collect()
+
             dpg.render_dearpygui_frame()
             if not dpg.is_dearpygui_running():
                 break
 
-            preprocessor.clear_lsh()
-
-    end_time = time.time()
-    print('Elapsed time:',end_time-start_time)
+        preprocessor.clear_lsh()
+        #break
 
     if dpg.is_dearpygui_running():
 
         dpg.set_value("filename", 'Generating synthetic samples...')
         dpg.render_dearpygui_frame()
     
-        preprocessor.generate_synthetic_samples( True, render_samples )        
+        preprocessor.generate_synthetic_samples( True, render_samples )
+        gc.collect()
+        dpg.render_dearpygui_frame()
+
         preprocessor.generate_synthetic_samples( False, render_samples )
+        gc.collect()
         dpg.render_dearpygui_frame()
 
         dpg.set_value("orderbook_samples", str(preprocessor.num_orderbook_samples())+"/"+str(total_samples) )
@@ -499,6 +480,9 @@ with Client(TOKEN, target=INVEST_GRPC_API) as client:
         dpg.render_dearpygui_frame()
 
     preprocessor.flush()
+
+    end_time = time.time()
+    print('Elapsed time:',end_time-start_time)
 
     while dpg.is_dearpygui_running():
         dpg.render_dearpygui_frame()

TkTrainAutoencoders.py
@@ -32,6 +32,7 @@ from TkModules.TkUI import TkUI
 from TkModules.TkOrderbookAutoencoder import TkOrderbookAutoencoder
 from TkModules.TkLastTradesAutoencoder import TkLastTradesAutoencoder
 from TkModules.TkTrainingHistory import TkAutoencoderTrainingHistory
+from TkModules.TkSSIM import hybrid_ms_ssim_l1_loss_1d
 
 #------------------------------------------------------------------------------------------------------------------------
 
@@ -216,6 +217,7 @@ class TkAutoencoderDataLoader():
         self._loading_thread = None
         return self._orderbook_samples, self._last_trades_samples           
          
+
 #------------------------------------------------------------------------------------------------------------------------
 
 TOKEN = os.environ["TK_TOKEN"]
@@ -238,6 +240,8 @@ orderbook_width = int(config['Autoencoders']['OrderBookWidth'])
 last_trades_width = int(config['Autoencoders']['LastTradesWidth'])
 orderbook_code_layer_size = int(config['Autoencoders']['OrderbookAutoencoderCodeLayerSize'])
 last_trades_code_layer_size = int(config['Autoencoders']['LastTradesAutoencoderCodeLayerSize'])
+orderbook_autoencoder_free_bits = float(config['Autoencoders']['OrderbookAutoencoderFreeBits'])
+last_trades_autoencoder_free_bits = float(config['Autoencoders']['LastTradesAutoencoderFreeBits'])
 orderbook_autoencoder_beta = float(config['Autoencoders']['OrderbookAutoencoderBeta'])
 last_trades_autoencoder_beta = float(config['Autoencoders']['LastTradesAutoencoderBeta'])
 orderbook_autoencoder_learning_rate = float(config['Autoencoders']['OrderbookAutoencoderLearningRate'])
@@ -250,20 +254,21 @@ orderbook_autoencoder = TkOrderbookAutoencoder(config)
 orderbook_autoencoder.to(cuda)
 if os.path.isfile(orderbook_model_path):
     orderbook_autoencoder.load_state_dict(torch.load(orderbook_model_path))
-orderbook_optimizer = torch.optim.RAdam( orderbook_autoencoder.parameters(), lr=orderbook_autoencoder_learning_rate, weight_decay=orderbook_autoencoder_weight_decay )
+orderbook_optimizer = torch.optim.Adam( orderbook_autoencoder.parameters(), lr=orderbook_autoencoder_learning_rate, weight_decay=orderbook_autoencoder_weight_decay )
 if os.path.isfile(orderbook_optimizer_path):
     orderbook_optimizer.load_state_dict(torch.load(orderbook_optimizer_path))
-orderbook_loss = torch.nn.BCELoss()
+
+orderbook_loss = lambda x,y: hybrid_ms_ssim_l1_loss_1d(x,y,win_size=13,alpha=0.95) # torch.nn.L1Loss() # torch.nn.BCELoss()
 orderbook_training_history = TkAutoencoderTrainingHistory(orderbook_history_path, history_size)
 
 last_trades_autoencoder = TkLastTradesAutoencoder(config)
 last_trades_autoencoder.to(cuda)
 if os.path.isfile(last_trades_model_path):   
     last_trades_autoencoder.load_state_dict(torch.load(last_trades_model_path))
-last_trades_optimizer = torch.optim.RAdam( last_trades_autoencoder.parameters(), lr=last_trades_autoencoder_learning_rate, weight_decay=last_trades_autoencoder_weight_decay )
+last_trades_optimizer = torch.optim.Adam( last_trades_autoencoder.parameters(), lr=last_trades_autoencoder_learning_rate, weight_decay=last_trades_autoencoder_weight_decay )
 if os.path.isfile(last_trades_optimizer_path):
     last_trades_optimizer.load_state_dict(torch.load(last_trades_optimizer_path))
-last_trades_loss = torch.nn.BCELoss()
+last_trades_loss = lambda x,y: hybrid_ms_ssim_l1_loss_1d(x,y,win_size=11,alpha=0.95) #torch.nn.BCELoss()
 last_trades_training_history = TkAutoencoderTrainingHistory(last_trades_history_path, history_size)
 
 data_loader = TkAutoencoderDataLoader(
@@ -371,6 +376,21 @@ with Client(TOKEN, target=INVEST_GRPC_API) as client:
 
     while dpg.is_dearpygui_running():
 
+        #def get_epoch_schedule_weight(smooth_epoch : float):
+        #    return 1.0 - math.exp( -0.0625 * smooth_epoch )
+
+        #orderbook_smooth_epoch = orderbook_training_history.get_smooth_epoch( data_loader.epoch_size() * data_loader.orderbook_training_batch_size() )
+        #orderbook_autoencoder_beta = float(config['Autoencoders']['OrderbookAutoencoderBeta']) * get_epoch_schedule_weight( orderbook_smooth_epoch )
+        #orderbook_autoencoder_weight_decay = float(config['Autoencoders']['OrderbookAutoencoderWeightDecay']) * get_epoch_schedule_weight( orderbook_smooth_epoch )
+        orderbook_optimizer.param_groups[0]['weight_decay'] = orderbook_autoencoder_weight_decay
+        orderbook_optimizer.param_groups[0]['lr'] = orderbook_autoencoder_learning_rate
+
+        #last_trades_smooth_epoch = orderbook_training_history.get_smooth_epoch( data_loader.epoch_size() * data_loader.last_trades_training_batch_size() )
+        #last_trades_autoencoder_beta = float(config['Autoencoders']['LastTradesAutoencoderBeta']) * get_epoch_schedule_weight( last_trades_smooth_epoch )
+        #last_trades_autoencoder_weight_decay = float(config['Autoencoders']['LastTradesAutoencoderWeightDecay']) * get_epoch_schedule_weight( last_trades_smooth_epoch )
+        last_trades_optimizer.param_groups[0]['weight_decay'] = last_trades_autoencoder_weight_decay
+        last_trades_optimizer.param_groups[0]['lr'] = last_trades_autoencoder_learning_rate
+
         orderbook_samples, last_trades_samples = data_loader.complete_loading()
 
         orderbook_input = torch.Tensor( list( itertools.chain.from_iterable(orderbook_samples) ) )
@@ -385,22 +405,21 @@ with Client(TOKEN, target=INVEST_GRPC_API) as client:
 
         y, y_mean, y_logvar = orderbook_autoencoder.forward( orderbook_input )
         z, z_mean, z_logvar = last_trades_autoencoder.forward( last_trades_input )
-        y.clamp( 0, 1 )
-        z.clamp( 0, 1 )
-        y_logvar.clamp( -10, 10 )
-        z_logvar.clamp( -10, 10 )
 
-        TkUI.set_series_from_tensor("x_axis_orderbook", "y_axis_orderbook","orderbook_series",orderbook_input,0)
-        TkUI.set_series_from_tensor("x_axis_orderbook_code", "y_axis_orderbook_code","orderbook_code_series",orderbook_autoencoder.code(),0)
-        TkUI.set_series_from_tensor("x_axis_orderbook_output", "y_axis_orderbook_output","orderbook_output_series",y,0)
+        #TkUI.set_series_from_tensor("x_axis_orderbook", "y_axis_orderbook","orderbook_series",orderbook_input,0)
+        #TkUI.set_series_from_tensor("x_axis_orderbook_code", "y_axis_orderbook_code","orderbook_code_series",orderbook_autoencoder.code(),0)
+        #TkUI.set_series_from_tensor("x_axis_orderbook_output", "y_axis_orderbook_output","orderbook_output_series",y,0)
 
-        TkUI.set_series_from_tensor("x_axis_last_trades", "y_axis_last_trades","last_trades_series",last_trades_input,0)
-        TkUI.set_series_from_tensor("x_axis_last_trades_code", "y_axis_last_trades_code","last_trades_code_series",last_trades_autoencoder.code(),0)
-        TkUI.set_series_from_tensor("x_axis_last_trades_output", "y_axis_last_trades_output","last_trades_output_series",z,0)
+        #TkUI.set_series_from_tensor("x_axis_last_trades", "y_axis_last_trades","last_trades_series",last_trades_input,0)
+        #TkUI.set_series_from_tensor("x_axis_last_trades_code", "y_axis_last_trades_code","last_trades_code_series",last_trades_autoencoder.code(),0)
+        #TkUI.set_series_from_tensor("x_axis_last_trades_output", "y_axis_last_trades_output","last_trades_output_series",z,0)
 
-        y_KLD_loss = -0.5 * torch.mean( torch.sum( 1 + y_logvar - y_mean.pow(2) - y_logvar.exp(), dim=1), dim=0 )
+        #y_KLD_loss = -0.5 * torch.mean( torch.sum( 1 + y_logvar - y_mean.pow(2) - y_logvar.exp(), dim=1), dim=0 )
+        y_KLD_loss = -0.5 * torch.sum( 1 + y_logvar - y_mean.pow(2) - y_logvar.exp(), dim=1)
 
-        y_recon_loss = orderbook_loss( y, orderbook_input )        
+        y_KLD_loss = torch.max( y_KLD_loss, torch.tensor( orderbook_autoencoder_free_bits, device=y_KLD_loss.device))
+
+        y_recon_loss = orderbook_loss( y, orderbook_input )
         y_loss = y_recon_loss + y_KLD_loss * orderbook_autoencoder_beta
         y_loss = y_loss.mean()
         orderbook_optimizer.zero_grad()
@@ -409,7 +428,11 @@ with Client(TOKEN, target=INVEST_GRPC_API) as client:
         y_KLD_loss_val = y_KLD_loss.mean().item()
         y_recon_loss_val = y_recon_loss.mean().item()
 
-        z_KLD_loss = -0.5 * torch.mean( torch.sum( 1 + z_logvar - z_mean.pow(2) - z_logvar.exp(), dim=1), dim=0 )
+        #z_KLD_loss = -0.5 * torch.mean( torch.sum( 1 + z_logvar - z_mean.pow(2) - z_logvar.exp(), dim=1), dim=0 )
+        z_KLD_loss = -0.5 * torch.sum( 1 + z_logvar - z_mean.pow(2) - z_logvar.exp(), dim=1)
+
+        z_KLD_loss = torch.max( z_KLD_loss, torch.tensor( last_trades_autoencoder_free_bits, device=z_KLD_loss.device))
+
         z_recon_loss = last_trades_loss( z, last_trades_input )        
         z_loss = z_recon_loss + z_KLD_loss * last_trades_autoencoder_beta
         z_loss = z_loss.mean()
@@ -441,11 +464,6 @@ with Client(TOKEN, target=INVEST_GRPC_API) as client:
         z, z_mean, z_logvar = last_trades_autoencoder.forward( last_trades_input )
         last_trades_autoencoder.train(True)
 
-        y.clamp( 0, 1 )
-        z.clamp( 0, 1 )
-        y_logvar.clamp( -10, 10 )
-        z_logvar.clamp( -10, 10 )
-
         TkUI.set_series_from_tensor("x_axis_orderbook", "y_axis_orderbook","orderbook_series",orderbook_input,0)
         TkUI.set_series_from_tensor("x_axis_orderbook_code", "y_axis_orderbook_code","orderbook_code_series",orderbook_autoencoder.code(),0)
         TkUI.set_series_from_tensor("x_axis_orderbook_output", "y_axis_orderbook_output","orderbook_output_series",y,0)
@@ -454,12 +472,16 @@ with Client(TOKEN, target=INVEST_GRPC_API) as client:
         TkUI.set_series_from_tensor("x_axis_last_trades_code", "y_axis_last_trades_code","last_trades_code_series",last_trades_autoencoder.code(),0)
         TkUI.set_series_from_tensor("x_axis_last_trades_output", "y_axis_last_trades_output","last_trades_output_series",z,0)
 
-        y_KLD_accuracy = -0.5 * torch.mean( torch.sum( 1 + y_logvar - y_mean.pow(2) - y_logvar.exp(), dim=1), dim=0 )
+        #y_KLD_accuracy = -0.5 * torch.mean( torch.sum( 1 + y_logvar - y_mean.pow(2) - y_logvar.exp(), dim=1), dim=0 )
+        y_KLD_accuracy = -0.5 * torch.sum( 1 + y_logvar - y_mean.pow(2) - y_logvar.exp(), dim=1)
+
         y_recon_accuracy = orderbook_loss( y, orderbook_input )        
         y_KLD_accuracy_val = y_KLD_accuracy.mean().item()
         y_recon_accuracy_val = y_recon_accuracy.mean().item()
 
-        z_KLD_accuracy = -0.5 * torch.mean( torch.sum( 1 + z_logvar - z_mean.pow(2) - z_logvar.exp(), dim=1), dim=0 )
+        #z_KLD_accuracy = -0.5 * torch.mean( torch.sum( 1 + z_logvar - z_mean.pow(2) - z_logvar.exp(), dim=1), dim=0 )
+        z_KLD_accuracy = -0.5 * torch.sum( 1 + z_logvar - z_mean.pow(2) - z_logvar.exp(), dim=1)
+
         z_recon_accuracy = last_trades_loss( z, last_trades_input )
         z_KLD_accuracy_val = z_KLD_accuracy.mean().item()
         z_recon_accuracy_val = z_recon_accuracy.mean().item()

TkTrainTimeSeries.py
@@ -208,8 +208,8 @@ if os.path.isfile(ts_model_path):
 ts_optimizer = torch.optim.RAdam( ts_model.parameters(), lr=learning_rate, weight_decay=weight_decay )
 if os.path.isfile(ts_optimizer_path):
     ts_optimizer.load_state_dict(torch.load(ts_optimizer_path))
-ts_loss = torch.nn.MSELoss(reduction="none") # torch.nn.HuberLoss(reduction="none") # 
-ts_accuracy = torch.nn.MSELoss(reduction="none") # torch.nn.HuberLoss(reduction = "none") # ts_accuracy = torch.nn.KLDivLoss(reduction = "batchmean", log_target=False)
+ts_loss = torch.nn.MSELoss(reduction="none") # torch.nn.HuberLoss(reduction="none") 
+ts_accuracy = torch.nn.MSELoss(reduction="none") # ts_accuracy = torch.nn.KLDivLoss(reduction = "batchmean", log_target=False)  # torch.nn.HuberLoss(reduction = "none") #
 ts_training_history = TkTimeSeriesTrainingHistory(ts_history_path, history_size)
 
 data_loader = TkTimeSeriesDataLoader(
@@ -327,8 +327,7 @@ with Client(TOKEN, target=INVEST_GRPC_API) as client:
         ts_optimizer.step()
         y_loss_val = y_loss.item()
 
-        mlp_input = ts_model.mlp_input()
-        TkUI.set_series_from_tensor("x_axis_lstm_training", "y_axis_lstm_training", "training_lstm_series", mlp_input, display_batch_id)
+        TkUI.set_series_from_tensor("x_axis_lstm_training", "y_axis_lstm_training", "training_lstm_series", ts_model.lstm_output(), display_batch_id)
 
         input_slice_size = ( input_slices[display_slice][1] - input_slices[display_slice][0] ) * prior_steps_count
         input_slice = ts_model.input_slice(display_slice)
@@ -377,9 +376,8 @@ with Client(TOKEN, target=INVEST_GRPC_API) as client:
         z_accuracy = ts_accuracy( y, target_code ).detach()
         z_accuracy = z_accuracy.mean()
         z_accuracy_val = z_accuracy.item()
-
-        mlp_input = ts_model.mlp_input()
-        TkUI.set_series_from_tensor("x_axis_lstm_test", "y_axis_lstm_test", "test_lstm_series", mlp_input, display_batch_id)
+        
+        TkUI.set_series_from_tensor("x_axis_lstm_test", "y_axis_lstm_test", "test_lstm_series", ts_model.lstm_output(), display_batch_id)
 
         dpg.render_dearpygui_frame()
 